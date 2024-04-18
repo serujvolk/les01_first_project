db359f92e9910a3ccc8dbf395048fbc60b1869bc
serujvolk <166744419+serujvolk@users.noreply.github.com>
2024-04-18 19:35:46 +0300

    Add files via upload

---
+++ Les1/src/com/SimpleConsoleOutput.java
@@ -0,0 +1,10 @@
+package com;
+
+public class SimpleConsoleOutput {
+
+	public static void main(String[] args) {
+
+		System.out.print("A \nB \nC \nD \nE");
+	}
+
+}
---
+++ Les1/src/edu/training/jc/simple_output/StairsOutput.java
@@ -0,0 +1,11 @@
+package edu.training.jc.simple_output;
+
+public class StairsOutput {
+
+	public static void main(String[] args) {
+		System.out.println(
+				"\"To wisely live your life, you don't need to know much\n\n\tJust remember two main rules for the  beginning:\n\n\t\tYou better starve, than eat whatever\n\n\tAnd better be alone, than with whoever.\"\n\nOmar Khayyam,  Rubaiyat");
+
+	}
+
+}
